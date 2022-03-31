e3707ec298747561228636a95025b7a843f49dd8
Dan Sazonov <p.294803@gmail.com>
2022-03-31 21:57:49 +0300

    Прифигачил кнопку

--- БРЕТФЭРД/main/main.ino
+++ БРЕТФЭРД/main/main.ino
@@ -5,11 +5,19 @@
 #include <DHT_U.h>
 
 #define DHT_PIN 2
+#define BUTTON_PIN 11
+#define LED_PIN 13
+ 
+bool buttonWasUp = true;
+bool dispEnabled = false;
 
 DHT dht(DHT_PIN, DHT11);
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 void setup() {
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(BUTTON_PIN, INPUT);
+  
   dht.begin();
   lcd.init();
   lcd.backlight();
@@ -18,6 +26,17 @@ void setup() {
 void loop() {
   float temp = dht.readTemperature();
   float hum = dht.readHumidity();
+
+  boolean buttonIsUp = !digitalRead(BUTTON_PIN);
+    if (buttonWasUp && !buttonIsUp) {  
+      delay(10);
+      buttonIsUp = digitalRead(BUTTON_PIN);
+    if (!buttonIsUp) {
+      dispEnabled = !dispEnabled;
+      digitalWrite(LED_PIN, dispEnabled);
+    }
+  }
+  buttonWasUp = buttonIsUp;
 
   if (isnan(hum) || isnan(temp)) {
     printData(0.00, 0.00);
