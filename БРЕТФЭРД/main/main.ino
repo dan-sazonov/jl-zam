64fe5cfeb4174f1e9daab99e597a7785efa37388
Dan Sazonov <p.294803@gmail.com>
2022-03-31 21:42:53 +0300

    Подружил дисплей с dht, запилил вывод

--- БРЕТФЭРД/main/main.ino
+++ БРЕТФЭРД/main/main.ino
@@ -1,38 +1,45 @@
-#include <Adafruit_Sensor.h>
+#include <Wire.h>
 #include <LiquidCrystal_I2C.h>
-#include <Wire.h> 
+#include <Adafruit_Sensor.h>
 #include <DHT.h>
 #include <DHT_U.h>
 
-#define DHT_PIN 2 
+#define DHT_PIN 2
 
 DHT dht(DHT_PIN, DHT11);
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 void setup() {
-  lcd.init();  
-  lcd.backlight();
   dht.begin();
+  lcd.init();
+  lcd.backlight();
 }
 
 void loop() {
-  float hum = dht.readHumidity();
   float temp = dht.readTemperature();
-  
-  if !(isnan(hum) || isnan(dht)) {
+  float hum = dht.readHumidity();
+
+  if (isnan(hum) || isnan(temp)) {
+    printData(0.00, 0.00);
+  } else {
     printData(temp, hum);
   }
+  
+  delay(2000);
 }
 
 int printData(float t, float h) {
-  lcd.setCursor(0,0);
+  lcd.setCursor(2, 0);
   lcd.print("Temp: ");
-  lcd.setCursor(10,0);
+  lcd.setCursor(10, 0);
   lcd.print(t);
+  lcd.setCursor(15, 0);
   lcd.print("C");
-  lcd.setCursor(0,1);
+
+  lcd.setCursor(0, 1);
   lcd.print("Humidity: ");
-  lcd.setCursor(10,1);
+  lcd.setCursor(10, 1);
   lcd.print(h);
+  lcd.setCursor(15, 1);
   lcd.print("%");
 }
