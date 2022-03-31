dc7cc21d6d3b2e6d8c58553a498e69530a823751
Dan Sazonov <p.294803@gmail.com>
2022-04-01 02:40:38 +0300

    Обработчик клика по кнопке

--- ЭЛИТА/main/main.ino
+++ ЭЛИТА/main/main.ino
@@ -1,13 +1,24 @@
-#define IRPIN 2
+#define IR_PIN 2
+#define BTN_PIN 11
+
+bool wasClicked = false;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(IRPIN, INPUT);
+  pinMode(IR_PIN, INPUT);
   pinMode(13, OUTPUT);
+  pinMode(BTN_PIN, INPUT_PULLUP);
 }
-
+int i = 0;
 void loop() {
-  bool s = digitalRead(IRPIN);
-  Serial.println(s);
-  digitalWrite(13, s);
+  bool hasMove = digitalRead(IR_PIN);
+
+  bool btnState = !digitalRead(BTN_PIN);
+  if (btnState && !wasClicked) {
+    wasClicked = true;
+  }
+  if (!btnState && wasClicked) wasClicked = false;  
+  
+  Serial.println(wasClicked);
+  digitalWrite(13, hasMove);
 }
