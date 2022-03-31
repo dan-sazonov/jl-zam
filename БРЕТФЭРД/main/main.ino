96c9879b49e1e061af8c07f1bd5bfb8160abc999
Dan Sazonov <p.294803@gmail.com>
2022-03-31 22:14:43 +0300

    Завел кнопку

--- БРЕТФЭРД/main/main.ino
+++ БРЕТФЭРД/main/main.ino
@@ -5,46 +5,44 @@
 #include <DHT_U.h>
 
 #define DHT_PIN 2
-#define BUTTON_PIN 11
+#define BUTTON_PIN 9
 #define LED_PIN 13
- 
-bool buttonWasUp = true;
-bool dispEnabled = false;
 
 DHT dht(DHT_PIN, DHT11);
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
+boolean buttonWasUp = true;
+boolean ledEnabled = false;
+
 void setup() {
-  pinMode(LED_PIN, OUTPUT);
-  pinMode(BUTTON_PIN, INPUT);
-  
   dht.begin();
   lcd.init();
   lcd.backlight();
+
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(BUTTON_PIN, INPUT);
 }
 
 void loop() {
   float temp = dht.readTemperature();
   float hum = dht.readHumidity();
 
-  boolean buttonIsUp = !digitalRead(BUTTON_PIN);
-    if (buttonWasUp && !buttonIsUp) {  
-      delay(10);
-      buttonIsUp = digitalRead(BUTTON_PIN);
-    if (!buttonIsUp) {
-      dispEnabled = !dispEnabled;
-      digitalWrite(LED_PIN, dispEnabled);
-    }
-  }
-  buttonWasUp = buttonIsUp;
-
   if (isnan(hum) || isnan(temp)) {
     printData(0.00, 0.00);
   } else {
     printData(temp, hum);
   }
-  
-  delay(2000);
+
+  boolean buttonIsUp = !digitalRead(BUTTON_PIN);
+  if (buttonWasUp && !buttonIsUp) {
+    delay(10);
+    buttonIsUp = digitalRead(BUTTON_PIN);
+    if (!buttonIsUp) {
+      ledEnabled = !ledEnabled;
+      digitalWrite(LED_PIN, ledEnabled);
+    }
+  }
+  buttonWasUp = buttonIsUp;
 }
 
 int printData(float t, float h) {
