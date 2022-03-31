6ecc3ac2d7e8fbc6ad126ada2e5e82b575c60cc7
Dan Sazonov <p.294803@gmail.com>
2022-04-01 02:24:37 +0300

    Отладка кнопки

---
+++ ЭЛИТА/vibro-test/vibro-test.ino
@@ -0,0 +1,17 @@
+#define BUTTON_PIN  11
+#define LED_PIN     13
+ 
+boolean buttonWasUp = true;  // была ли кнопка отпущена?
+boolean ledEnabled = false;  // включен ли свет?
+ 
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+}
+ 
+void loop()
+{
+  Serial.println(digitalRead(BUTTON_PIN));
+}
