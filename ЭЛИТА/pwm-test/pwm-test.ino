2c89c977020e63048361cba932cd203707c59239
Dan Sazonov <p.294803@gmail.com>
2022-04-01 04:16:44 +0300

    Тест шима

---
+++ ЭЛИТА/pwm-test/pwm-test.ino
@@ -0,0 +1,30 @@
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  analogWrite(6, 5);
+  delay(300);
+  analogWrite(6, 25);
+  delay(300);
+  analogWrite(6, 50);
+  delay(300);
+  analogWrite(6, 75);
+  delay(300);
+  analogWrite(6, 100);
+  delay(300);
+  analogWrite(6, 125);
+  delay(300);
+  analogWrite(6, 150);
+  delay(300);
+  analogWrite(6, 175);
+  delay(300);
+  analogWrite(6, 200);
+  delay(300);
+  analogWrite(6, 225);
+  delay(300);
+  analogWrite(6, 250);
+  delay(300);
+  analogWrite(6, 255);
+}
