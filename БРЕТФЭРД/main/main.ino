b7a25f897202c6e5cb3129eaf8e3df8b07d3d80d
Dan Sazonov <p.294803@gmail.com>
2022-04-01 05:25:50 +0300

    Добавил коменты в код Бретфэрда

--- БРЕТФЭРД/main/main.ino
+++ БРЕТФЭРД/main/main.ino
@@ -4,15 +4,17 @@
 #include <DHT.h>
 #include <DHT_U.h>
 
-#define DHT_PIN 2
-#define BUTTON_PIN 11
+#define DHT_PIN 2   // пин к которому подключен dht
+#define BUTTON_PIN 11   // пин к которму подключена сенснорная кнопка
 
+// инициализируем классы для работы с библиотеками
 DHT dht(DHT_PIN, DHT11);
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
-bool buttonWasUp = true;
-bool dispEnabled = false;
-bool shownAlert = false;
+// инициализируем глобальные переменные:
+bool buttonWasUp = true;    // была ли нажата кнопка
+bool dispEnabled = false;   // активен ли дисплей
+bool shownAlert = false;    // было ли показано уведомление
 unsigned long timing;
 
 void setup() {
@@ -24,33 +26,34 @@ void setup() {
 }
 
 void loop() {
-  bool buttonIsUp = !digitalRead(BUTTON_PIN);
+  bool buttonIsUp = !digitalRead(BUTTON_PIN);   // считываем сигнал с пина кнопки
   if (buttonWasUp && !buttonIsUp) {
     buttonIsUp = digitalRead(BUTTON_PIN);
     if (digitalRead(BUTTON_PIN)) {
-      dispEnabled = !dispEnabled;
+      dispEnabled = !dispEnabled;   // если кнопка была нажата, меняем значение флага
       while (digitalRead(BUTTON_PIN)) {}
-      dispWorking(dispEnabled);
+      dispWorking(dispEnabled);   // меняем состояние дисплея по нажатию на кнопку
     }
   }
-  buttonWasUp = buttonIsUp;
+  buttonWasUp = buttonIsUp;   // сохраняем в переменную текущее состояние кнопки
 
   if (millis() - timing > 2000) {
+    // опрашиваем каждые 2 секунды dht. чаще нельзя
     float temp = dht.readTemperature();
     float hum = dht.readHumidity();
 
     if (!(isnan(hum) || isnan(temp))) {
-      if ((temp > 24.0 || hum < 40.0) && !shownAlert) {
+      if ((temp > 24.0 || hum < 40.0) && !shownAlert) {   // если некомфортные значения и уведомление еще не было показано...
         lcd.clear();
         lcd.setCursor(0, 0);
-        lcd.print("OPEN THE WINDOW!");
-        shownAlert = true;
+        lcd.print("OPEN THE WINDOW!");    // показываем уведомление
+        shownAlert = true;  // меняем значение флага
       } else if (shownAlert) {
-        printData(temp, hum);
+        printData(temp, hum);   // если уведомление уже было показано, а значения не комфортные, выводим их
         shownAlert = false;
-      } else printData(temp, hum);
-    } else printData(0.0, 0.0);
-    timing = millis();
+      } else printData(temp, hum);    // если значения нормальные, выводим их
+    } else printData(0.0, 0.0);   // если не удалось считать данные, выводим нули
+    timing = millis();    // обновляем счетчик таймера
   }
 }
 
