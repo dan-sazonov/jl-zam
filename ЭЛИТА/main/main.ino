588bd30dca7df654765a2df8fad3122ec14a76e0
Dan Sazonov <p.294803@gmail.com>
2022-04-01 03:12:27 +0300

    Обработка сигнала с ИК

--- ЭЛИТА/main/main.ino
+++ ЭЛИТА/main/main.ino
@@ -1,24 +1,24 @@
 #define IR_PIN 2
 #define BTN_PIN 11
 
-bool wasClicked = false;
+bool ledState = false;
+bool lastPir = false;
 
 void setup() {
   Serial.begin(9600);
   pinMode(IR_PIN, INPUT);
   pinMode(13, OUTPUT);
-  pinMode(BTN_PIN, INPUT_PULLUP);
+  pinMode(BTN_PIN, INPUT);
 }
-int i = 0;
+
 void loop() {
   bool hasMove = digitalRead(IR_PIN);
 
-  bool btnState = !digitalRead(BTN_PIN);
-  if (btnState && !wasClicked) {
-    wasClicked = true;
+  if (lastPir != hasMove) {
+    ledState = hasMove;
+    lastPir = hasMove;
   }
-  if (!btnState && wasClicked) wasClicked = false;  
-  
-  Serial.println(wasClicked);
-  digitalWrite(13, hasMove);
+  //  Serial.println(wasClicked);
+    digitalWrite(13, ledState);
+    
 }
