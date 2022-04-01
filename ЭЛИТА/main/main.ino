f040504ce0dc0cdb2499930466b7db14070b4870
Dan Sazonov <p.294803@gmail.com>
2022-04-01 05:36:44 +0300

    Добавил коменты в код элиты

--- ЭЛИТА/main/main.ino
+++ ЭЛИТА/main/main.ino
@@ -1,15 +1,14 @@
-#define IR_PIN 2
-#define BTN_PIN 11
-#define LED_PIN 6
-#define PHOTO_PIN A1
-#define POT_PIN A0
+#define IR_PIN 2    // пин датчика движения
+#define BTN_PIN 11    // пин кнопки
+#define LED_PIN 6   // пин светодиода
+#define PHOTO_PIN A1    // пин фоторезистора
+#define POT_PIN A0    // пин потенциометра
 
-bool ledState = false;
-bool lastPir = false;
-bool flag = false;
+bool ledState = false;    // состояние светодиода (вкл/выкл)
+bool lastPir = false;   // прошлое состояние датчика движения
+bool flag = false;    // фалг кнопки с ее прошлым состоянием
 
 void setup() {
-  Serial.begin(9600);
   pinMode(IR_PIN, INPUT);
   pinMode(LED_PIN, OUTPUT);
   pinMode(BTN_PIN, INPUT);
@@ -18,30 +17,31 @@ void setup() {
 }
 
 void loop() {
-  bool hasMove = digitalRead(IR_PIN);
-  int photo = analogRead(PHOTO_PIN);
-  int potSignal = analogRead(POT_PIN);
+  bool hasMove = digitalRead(IR_PIN);   // считываем, было ли движение
+  int photo = analogRead(PHOTO_PIN);    // считываем сигнал с фоторезистора
+  int potSignal = analogRead(POT_PIN);    // считываем сигнал с потенциометра
 
   if (lastPir != hasMove) {
-    ledState = hasMove;
-    lastPir = hasMove;
+    ledState = hasMove;   // если состояние движения изменилось, меняем состояние светодиода
+    lastPir = hasMove;    // и обновляем флаг
   }
 
-  bool btnState = digitalRead(11);
+  bool btnState = digitalRead(BTN_PIN);   // считываем сигнал с кнопки
   if (btnState && !flag) {
-    flag = true;
-    ledState = !ledState;
+    flag = true;    // если кнопка была нажата, меняем заначение флага
+    ledState = !ledState;   // и инвертируем состояние светодиода
   }
-  if (!btnState && flag) flag = false;
+  if (!btnState && flag) flag = false;    // если кнопка была отпущена, меняем значение флага  
 
-  byte maxBright = map(potSignal, 0, 1023, 1, 255);
-
-  byte bright = map(photo, 0, 1023, maxBright, 5);
-  bright = constrain(bright, 0, 255);
-  ledOn(bright);
+  byte maxBright = map(potSignal, 0, 1023, 1, 255);  // вычисляем максимальную яркость, преобразовываем диапазон 0...1023 в 1..255
+  byte bright = map(photo, 0, 1023, maxBright, 5);    // вычисляем нужную яркость, преобразовываем диапазон 0...1023 в диапазон от 5 до максимального значения
+  // поскольку исходному значению 0 должна соответствовать максимальная яркость, аргументы нового диапазона указаны в обратном порядке
+  bright = constrain(bright, 0, 255);   // для надежности обрезаем диапазон
+  
+  ledOn(bright);    // запускаем светодиод с определенной яркостью
 }
 
 byte ledOn(byte bright) {
-  if (!ledState) bright = 0;
+  if (!ledState) bright = 0;    // если статус 0, яркость ставим 0
   analogWrite(LED_PIN, bright);
 }
